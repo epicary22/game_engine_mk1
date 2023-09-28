14c496e9e263afeb1a6c1e413ac9f419bed3eb43
epicary22 <epicary22@gmail.com>
2023-09-28 19:43:10 +0000

    minor hitbox changes

--- hitbox/circle_hitbox.py
+++ hitbox/circle_hitbox.py
@@ -1,5 +1,7 @@
 import pygame as pg
-from hitbox import Hitbox
+from hitbox.hitbox import Hitbox
+from hitbox.rect_hitbox import RectHitbox
+import math
 
 
 class CircleHitbox(Hitbox):
@@ -12,14 +14,15 @@ class CircleHitbox(Hitbox):
 		center: pg.Vector2,
 		is_on: bool = True,
 		rounding: bool = True
-		):
+		) -> None:
 		"""
+		Defines a circular hitbox.
 		:param radius: The radius of the circle.
 		:param center: The center point of the circle.
 		:param is_on: Whether the hitbox is active or not.
 		:param rounding: Whether the hitbox floor-rounds all of its values or not.
 		"""
 		super().__init__(dimensions=pg.Vector2(radius * 2, radius * 2), center=center, is_on=is_on, rounding=rounding)
-		
 
-# TODO class defining a circular hitbox.
+	def distance_to(self, other_circle: CircleHitbox) -> bool:
+		pass  # TODO find distance function between a circle and a circle, and a circle and a rect.
